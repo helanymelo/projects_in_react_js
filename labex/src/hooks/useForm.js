0c0f83ef3e3244220651733c4f787652a336a32f
Helany <helany.f.melo@gmail.com>
2022-01-04 19:53:51 -0300

    adicionando useForm Login

--- labex/src/hooks/useForm.js
+++ labex/src/hooks/useForm.js
@@ -4,10 +4,13 @@ import { useState } from "react";
 export const useForm = (initialValues) => {
   const [form, setForm] = useState(initialValues);
 
-  const onChange = (value, name) => {
-    setForm({ ...form, [name]: value });
+  const onChangeInput = (event) => {
+    const newValue = event.target.value
+    const fieldName = event.target.name
+    setForm({ ...form, [fieldName]: newValue });
   };
 
+  return [form, onChangeInput]
   
 
 }
--- labex/src/pages/LoginPage/LoginPage.js
+++ labex/src/pages/LoginPage/LoginPage.js
@@ -2,7 +2,6 @@ import { Button, TextField, Grid, Paper, Avatar} from "@material-ui/core";
 import React from "react";
 import LockRoundedIcon from '@mui/icons-material/LockRounded';
 import {useForm} from "../../hooks/useForm";
-import axios from 'axios';
 
 
 
@@ -10,46 +9,37 @@ const LoginPage =()=>{
     const paperStyle={padding:20, height:'70vh', width:'300px', margin:'50px auto'}
     const avatarStyle={backgroundColor:'#2b34a1'}
     const btnStyle ={margin:'20px auto'}
-    const {form, onChange} = useForm({email: "", password: ""})
+    const {form, onChangeInput} = useForm({email:"", password:""})
 
-    
 
-    const handleInputChange = (event) => {
-        const {value, name} = event.target
-
-        onChange(value, name)
-    }
-
-    const handleSubmission = (event) => {
+    const onSubmitLogin = (event) =>{
         event.preventDefault()
     }
 
-        
-
-   
-    return(
+  
+   return(
         <>
-        <Grid onSubmit={ handleSubmission}>
+        <Grid onSubmit={onSubmitLogin}>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
                 <Avatar style={avatarStyle}><LockRoundedIcon/></Avatar>
                     <h3>Sign In</h3>
                 </Grid>
-                <TextField label="E-mail" fullWidth required
-                type="email"
-                onChange={handleInputChange}
-                value={form.email}
-                name='email'/>
+                <TextField label={"Email"} 
+                type={"email"}
+                onChange={onChangeInput}
+                value={form["email"]}
+                name={"email"}/>
 
-                <TextField label="Password" fullWidth required
-                type='password'
-                 onChange={handleInputChange}
-                 value={form.password}
-                 name='password'/>
+                <TextField label={"password"} 
+                type={"password"}
+                 onChange={onChangeInput}
+                 value={form["password"]}
+                 name={"password"}/>
                 <Grid>
-                    <Button type="submit" 
-                        color="primary" 
-                        variant="contained" 
+                    <Button type={"submit"} 
+                        color={"primary"} 
+                        variant={"contained"} 
                         fullWidth 
                         style={btnStyle}>
                             Acessar

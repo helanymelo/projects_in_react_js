2f56e13daa763bbbce8c0ee0e6376f0e417f5283
Helany <helany.f.melo@gmail.com>
2022-01-04 08:27:03 -0300

    adicionando pages

---
+++ labex/src/pages/TripDetailPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TripDetailPage = () => {
+    return (
+        <div>
+            <h1>TripDetailPage</h1>
+        </div>
+    )
+}
+
+export default TripDetailPage

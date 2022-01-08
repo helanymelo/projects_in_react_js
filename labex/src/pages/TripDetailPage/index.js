447f2e06e96cc7f0afcabfed70a019ec4217de53
Helany <helany.f.melo@gmail.com>
2022-01-07 21:21:53 -0300

    resolvendo conflitos

--- labex/src/pages/TripDetailPage/index.js
+++ labex/src/pages/TripDetailPage/index.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
+ 
 import React from 'react'
 
 const TripDetailPage = () => {
     return (
-        <div>
+        
             <h1>TripDetailPage</h1>
-=======
 import React from "react";
 import PageTitle from "../../components/PageTitle";
 import CandidateList from "./CandidateList";
@@ -21,13 +20,8 @@ const TripPage =()=>{
             <TripInfo/>
             <CandidateList/>
             </ContentContainer>
->>>>>>> b8dae96d896b26268f748535b525c7b5d96fd1c1
         </div>
     )
 }
 
-<<<<<<< HEAD
-export default TripDetailPage
-=======
 export default TripPage;
->>>>>>> b8dae96d896b26268f748535b525c7b5d96fd1c1

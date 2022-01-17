6ff1c587fbf7e6fa9f96653f2e94828980ae6e79
helany <helany.f.melo@gmail.com>
2022-01-17 20:58:44 -0300

    adicionando exercicios de reactjs

---
+++ grocery/src/App.js
@@ -0,0 +1,16 @@
+import Content from './Content/Content';
+import Footer from './Footer/Footer';
+import Header from './Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
